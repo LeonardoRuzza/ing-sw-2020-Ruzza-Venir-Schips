16d05bbbcd23b41e6d15b3fd0a33bbc8d6d4bfa3
Valerio <valerio_schips@hotmail.it>
2020-03-15 16:18:21 +0100

    definizione costruttore

--- src/main/java/it/polimi/ingsw/model/Board.java
+++ src/main/java/it/polimi/ingsw/model/Board.java
@@ -1,11 +1,19 @@
 package it.polimi.ingsw.model;
 
 public class Board {
+
+    final int boardSide = 5;
     private Cell[][][] cell;
 
 
     public Board() {
-       // for ...
+       for(int z = 0; z < 5; z++){
+           for(int y = 0; y < 5; y++){
+               for(int x = 0; x < 5; x++){
+                    cell[x][y][z] = new Cell(x, y, z);
+               }
+           }
+       }
     }
 
     public Block blockInCell(int x, int y) {
