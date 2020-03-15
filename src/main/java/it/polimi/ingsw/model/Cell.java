e51eb4e6ab70ec3fc4461f135cba35d1c46f1ef6
Valerio <valerio_schips@hotmail.it>
2020-03-15 16:14:02 +0100

    definizione attributi classe, metodi e costruttore

--- src/main/java/it/polimi/ingsw/model/Cell.java
+++ src/main/java/it/polimi/ingsw/model/Cell.java
@@ -1,4 +1,33 @@
 package it.polimi.ingsw.model;
 
 public class Cell {
+
+    private int xCoord;
+    private int yCoord;
+    private int zCoord;
+    private Worker workerInCell;
+    private Block blockInCell;
+
+    public Cell(int x, int y, int z){
+        this.xCoord = x;
+        this.yCoord = y;
+        this.zCoord = z;
+        workerInCell = null;
+        blockInCell = null;
+    }
+    public Worker getWorker(){
+        return workerInCell;
+    }
+
+    public Block getBlock(){
+        return blockInCell;
+    }
+
+    public Block addBlock(){
+
+    }
+
+    public Boolean moveWorkerInto(Worker w){
+
+    }
 }
