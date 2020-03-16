81e6ccccce17af0a6b4a247cb1f793aa86dc37d9
Valerio <valerio_schips@hotmail.it>
2020-03-16 13:28:40 +0100

    Modifica Card da terminare

--- src/main/java/it/polimi/ingsw/model/Card.java
+++ src/main/java/it/polimi/ingsw/model/Card.java
@@ -3,9 +3,9 @@ package it.polimi.ingsw.model;
 public class Card {
 
     private int number;
-    private String name;
-    private String description;
-
+    static final String names[] = ;
+    static final String descriptions[] = ;
+    static final activationPeriod actPeriods[] = ;
     // Da valutare come gestire le varie carte, se con un Enum, o con uno switch multiplo all'interno della classe card
 
     private enum activationPeriod{}; // Da finire
