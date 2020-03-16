9aa3a0a9ecff7d7522f0086fdad1d9f3a2c9a8f6
Edoardo Venir <10570524@polimi.it>
2020-03-16 10:20:16 +0100

    Changed builder for class Board

--- src/main/java/it/polimi/ingsw/model/Board.java
+++ src/main/java/it/polimi/ingsw/model/Board.java
@@ -3,20 +3,21 @@ package it.polimi.ingsw.model;
 public class Board {
 
     final int boardSide = 5;
-    private Cell[][][] cell;
+    private Cell[][][] cells;
 
 
     public Board() {
+       cells = new Cell[5][5][5];
        for(int z = 0; z < 5; z++){
            for(int y = 0; y < 5; y++){
                for(int x = 0; x < 5; x++){
-                    cell[x][y][z] = new Cell(x, y, z);
+                    cells[x][y][z] = new Cell(x, y, z);
                }
            }
        }
     }
 
-    public Block blockInCell(int x, int y) {
+    public Block blockInCell(int x, int y) {   // Da modificare
         if(blockInCell(x, y) != null){
             return blockInCell(x, y);
         }else{
@@ -24,7 +25,7 @@ public class Board {
         }
     }
 
-    public Worker workerInCell(int x, int y) {
+    public Worker workerInCell(int x, int y) {  // Da modificare
         if(workerInCell(x, y) != null){
             return workerInCell(x, y);
         }else{
