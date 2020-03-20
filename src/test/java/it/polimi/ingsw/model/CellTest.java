44c5152b942a069425a3ce609ba0c70b95c6cfff
Edoardo Venir <10570524@polimi.it>
2020-03-20 10:16:08 +0100

    Complete tests fo class Cell

--- src/test/java/it/polimi/ingsw/model/CellTest.java
+++ src/test/java/it/polimi/ingsw/model/CellTest.java
@@ -4,24 +4,43 @@ import org.junit.*;
 
 public class CellTest {
 
-    private Cell cell;
+    private Cell cell = new Cell(3, 4, 3);
+    private Cell c1 = new Cell(1, 1, 0);
+    private Cell c2 = new Cell(1, 1, 1);
+    private Cell c3 = new Cell(1, 1, 2);
+    private Cell c4 = new Cell(1, 1, 3);
 
 
     @Test
     public void testBuilder() {
 
-        cell = new Cell(3,4,5);
-        Assert.assertTrue("Error", cell.getxCoord() == 3 && cell.getyCoord() == 4 && cell.getzCoord() == 5);
-
+        Assert.assertTrue("Error", cell.getxCoord() == 3 && cell.getyCoord() == 4 && cell.getzCoord() == 3);
+        Assert.assertNull(cell.getBlock());
+        Assert.assertNull(cell.getWorker());
     }
 
-    @BeforeClass
-    public static void beforeClass() throws Exception {
+    @Test
+    public void testAddBlock() { // Verifico che venga costruito il blocco giusto in base all'altezza della cella
+        c1.addBlock();
+        c2.addBlock();
+        c3.addBlock();
+        c4.addBlock();
+        Assert.assertEquals(c1.getBlock(), Block.B1);
+        Assert.assertEquals(c2.getBlock(), Block.B2);
+        Assert.assertEquals(c3.getBlock(), Block.B3);
+        Assert.assertEquals(c4.getBlock(), Block.DORSE);
 
+        cell.addBlock();
+        Assert.assertNotEquals(cell.getBlock(), Block.B1);
+        Assert.assertNotEquals(cell.getBlock(), Block.B2);
+        Assert.assertNotEquals(cell.getBlock(), Block.B3);
+        Assert.assertEquals(cell.getBlock(), Block.DORSE);
+
+        Assert.assertEquals(cell.addBlock(), Block.DORSE);  // Provo a ricostruire sulla stessa cella
     }
 
-    @AfterClass
-    public static void afterClass() throws Exception {
+    @Test
+    public void testMoveWorkerInto() {
 
     }
 }
