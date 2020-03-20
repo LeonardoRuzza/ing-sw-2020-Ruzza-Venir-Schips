813b205c3af509b9a8c9c3ef9611b1b1843be5b3
Leonardo Ruzza <10608001@polimi.it>
2020-03-20 10:12:54 +0100

    Implemanted PlayerMinotaur.

--- src/main/java/it/polimi/ingsw/model/PlayerMinotaur.java
+++ src/main/java/it/polimi/ingsw/model/PlayerMinotaur.java
@@ -8,8 +8,19 @@ public class PlayerMinotaur extends Player {
 
     @Override
     public boolean selectedWorkerMove(int x, int y){
+        Worker tempWorker;
+        int x0, y0, xFinal, yFinal;
         if(super.selectedWorkerMove(x, y)){return true;}
-
-
+        tempWorker=match.checkMove(x, y, selectedWorker);
+        if(tempWorker!=null && !tempWorker.equals(selectedWorker)){                                                          //ora serve capire la direzione dello spostamento per poter coerentemente spostarci il worker avversario
+            x0=selectedWorker.getCell().getxCoord();
+            y0=selectedWorker.getCell().getyCoord();
+            xFinal=x-x0;
+            yFinal=y-y0;
+            if(match.checkBuild(x+xFinal, y+yFinal, tempWorker)){                                                           //ritorna true se non c'è sopra una cupola o un worker! Perfetto!
+                if(match.forceMove(x+xFinal, y+yFinal , tempWorker) && match.forceMove(x, y, selectedWorker)){return true;}
+            }
+        }
+        return false;
     }
 }
---
+++ src/test/java/it/polimi/ingsw/model/PlayerMinotaurTest.java
@@ -0,0 +1,30 @@
+package it.polimi.ingsw.model;
+
+import org.junit.*;
+
+public class PlayerMinotaurTest {
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        
+    }
+
+    @AfterClass
+    public static void afterClass() throws Exception {
+        
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        
+    }
+
+    @Test
+    public void testSelectedWorkerMove() {
+
+    }
+}
