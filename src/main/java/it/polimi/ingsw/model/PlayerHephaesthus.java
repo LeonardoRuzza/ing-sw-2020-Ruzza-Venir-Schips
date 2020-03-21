3548bdcf97941e5c45e21de0c27d7de5048b618a
Leonardo Ruzza <10608001@polimi.it>
2020-03-21 19:18:21 +0100

    Fixed PlayerHephaesthus.

--- src/main/java/it/polimi/ingsw/model/PlayerHephaesthus.java
+++ src/main/java/it/polimi/ingsw/model/PlayerHephaesthus.java
@@ -2,32 +2,42 @@ package it.polimi.ingsw.model;
 
 public class PlayerHephaesthus extends Player {
     private boolean firstBuild=true;
+    private int x0=-1, y0=-1;
     public PlayerHephaesthus(String nickname, int number, Card card, Match match, Worker.Color color) {
         super(nickname, number, card, match, color);
     }
 
     protected void resetTurn(){         //reset esplicito necessario nel caso venga eseguita una sola costruzione da Hephaesthus
         firstBuild=true;
+        x0=-1;
+        y0=-1;
+
     }
 
     @Override
     public boolean selectedWorkerBuild(int x, int y){
         if(firstBuild){
-            if(super.selectedWorkerMove(x,y)){
+            if(super.selectedWorkerBuild(x,y)){
                 firstBuild=false;
+                x0=x;
+                y0=y;
                 return true;
             }
             else return false;
         }
-        else if(super.selectedWorkerBuild(x,y)){              //costruisco indipendentemente se è una cupola perchè in questo caso la checkBuild seguente ritorna false e allora applicherei la removeBlock
-            if(!(match.checkBuild(x,y,selectedWorker))){
-                match.removeBlock(x,y);
-                resetTurn();
-                return false;
-            }
-            else{
-                resetTurn();
-                return true;
+        else {
+            if(x0 ==x && y0==y){                                 //verifico che vogli costruire sulla stessa cella precedente
+                if(super.selectedWorkerBuild(x,y)){              //costruisco indipendentemente se è una cupola perchè in questo caso la checkBuild seguente ritorna false e allora applicherei la removeBlock
+                    if(!(match.checkBuild(x,y,selectedWorker))){
+                        match.removeBlock(x,y);
+                        resetTurn();
+                        return false;
+                    }
+                    else{
+                        resetTurn();
+                        return true;
+                    }
+                }
             }
         }
         return false;
