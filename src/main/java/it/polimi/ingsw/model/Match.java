ed87f5451fcb3e70e7cb979ed238ccb99cee59ce
ValerioSchips <valerio.schips@mail.polimi.it>
2020-03-21 13:10:00 +0100

    Implentation of Match functions and test

--- src/main/java/it/polimi/ingsw/model/Match.java
+++ src/main/java/it/polimi/ingsw/model/Match.java
@@ -1,44 +1,145 @@
 package it.polimi.ingsw.model;
 
+import org.jetbrains.annotations.NotNull;
+
 public class Match {
 
+    final int maxPlayer = 3;
     private int ID;
     private int numberOfPlayers;
     private enum stateOfMatch{};
     private Player playingNow;
-    private Player[] players;
+    protected Player[] players;
     private Board board;
-
 
     public Match(int ID, int numberOfPlayers) {  // Da valutare quali parametri passare, per creazione partita
         this.ID = ID;
         this.numberOfPlayers = numberOfPlayers;
-    }
-    /*public Player checkWin() {
-
+        this.board = new Board();
     }
 
-    public Player checkLoser() {
-
-    }
-
-    protected Worker checkMove (int x, int y, Worker w) { //controlli regole generali validi per tutti
-
-    }
-
-    protected Block checkBuild (int x, int y, Worker w) {
-
+    public int getNumberOfPlayers(){
+        return numberOfPlayers;
     }
 
 
-    private Player nextPlayer(){
-
+    public boolean checkWin(@NotNull Worker w) {
+        if (w.getOldLocation() == null || w.getCell() == null){
+            return checkSuperWin(playingNow);
+        }
+        if (w.getCell().getzCoord() == 3 && !(w.getCell().equals(w.getOldLocation())) && w.getOldLocation().getzCoord() != 3){
+            return checkSuperWin(playingNow);
+        }
+        return checkSuperWin(playingNow);
     }
 
-    protected boolean forceMove(int x,int y, Worker w){   //controlli influenzati da limitazioni avversarie
+    public boolean checkSuperWin(Player p){
+        return false;
+    }
+
+    public boolean checkLimitWin(Player p){
+        return false;
+    }
+
+    //va fatta a inizio turno
+    public Player checkLoserMove(@NotNull Worker w) {
+        for(int x = w.getCell().getxCoord()-1; x < w.getCell().getxCoord()+2; x++){
+            if(x > 4 || x < 0){
+                continue;
+            }
+            for(int y = w.getCell().getyCoord()-1; y < w.getCell().getyCoord()+2; y++){
+                if(y > 4 || y < 0 || x == y){
+                    continue;
+                }
+                if(board.getLastBusyCell(x, y).getBlock() != Block.DORSE){
+                    if(board.getDistance(w.getCell(), board.getLastBusyCell(x, y))[2] < 2){
+                        if((board.getLastBusyCell(x, y).getWorker()) == null){ // da modificare per carta speciale
+                            return null;
+                        }
+                    }
+                }
+            }
+        }
+        return playingNow;
+    }
+
+    //va fatta sul turno dopo lo spostamento
+    public Player checkLoserBuild(@NotNull Worker w) {
+        for(int x = w.getCell().getxCoord()-1; x < w.getCell().getxCoord()+2; x++){
+            if(x > 4 || x < 0){
+                continue;
+            }
+            for(int y = w.getCell().getyCoord()-1; y < w.getCell().getyCoord()+2; y++){
+                if(y > 4 || y < 0 || x == y){
+                    continue;
+                }
+                if(board.getLastBusyCell(x, y).getBlock() != Block.DORSE){
+                    if((board.getLastBusyCell(x, y).getWorker()) == null){ // da modificare per carta speciale
+                        return null;
+                    }
+                }
+            }
+        }
+        return playingNow;
+    }
+
+    //\result == null ==> error
+    //\result == playerWorker ==> done
+    //\result == foeWorker ==> do something
+    protected Worker checkMove (int x, int y, Worker w) { //controlli regole generali validi per
+        Cell moveToCell = board.getLastBusyCell(x,y);
+        if(moveToCell.getWorker() != null){
+            return moveToCell.getWorker();
+        }else if (moveToCell.getBlock() == Block.DORSE){
+            return null;
+        }
+        if(w.getCell() != null){
+            if(w.getCell().equals(moveToCell)){
+                return null;
+            }
+            int[] distance = board.getDistance(w.getCell(), moveToCell);
+            if(distance[0] > 1 || distance[1] > 1 || distance[2] > 1){
+                return null;
+            }
+        }
+        return w;
+    }
+
+    protected boolean checkBuild (int x, int y, @NotNull Worker w) {//ricordati che nel caso di costruzione andari una cella sopra a getlastbusycell
+        Cell buildToCell = board.getLastBusyCell(x, y);
+        if(w.getCell().equals(buildToCell)){
+            return false;
+        }
+        if(buildToCell.getWorker() != null){
+            return false;
+        }else if (buildToCell.getBlock() == Block.DORSE){
+            return false;
+        }
+        int[] distance = board.getDistance(w.getCell(), buildToCell);
+        //cambiare zeta
+        return distance[0] <= 1 && distance[1] <= 1;
+    }
+
+    protected Player nextPlayer(){
+        if(players[numberOfPlayers-1].equals(playingNow) || playingNow == null){ // caso in cui il giocatore è l'ultimo caso in cui si inizia la partita
+            playingNow = players[0];
+        }else{
+            for (int x = 0; x < numberOfPlayers; x++){
+            if (players[x].equals(playingNow)) {
+                    playingNow = players[x+1];
+                    break;
+                }
+            }
+        }
+        return playingNow;
+    }
+
+    //imposto da vincoli esterni in caso di esito positivo x e y saranno gli stessi di checkmove e checkbuild
+    protected boolean forceMove(int x,int y, Worker w){
+        return board.forceMove(board.getLastBusyCell(x, y), w);
     }
 
     protected boolean forceBuild(int x, int y, Worker w){
+        return ((board.forceBuild(board.getFirstBuildableCell(x, y))) != null);
     }
-    */
 }
--- src/test/java/it/polimi/ingsw/model/MatchTest.java
+++ src/test/java/it/polimi/ingsw/model/MatchTest.java
@@ -1,4 +1,4 @@
-/*package it.polimi.ingsw.model;
+package it.polimi.ingsw.model;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -34,29 +34,47 @@ public class MatchTest {
     public void testCheckLoserBuild() {
         //per test serve player
     }
-
-    final int[] startLocation = new int[]{0,0};
-
     @Test
-    public void testCheckMove() {
+    public void testMovementLocateMovement() {
+        int[] location = new int[]{0,0};
         //testPosizionamento inizio partita
-        Assert.assertTrue("Error checkMove first locate", match.checkMove(startLocation[0],startLocation[1],testWorkerOne).equals(testWorkerOne));
+        Assert.assertTrue("Error checkMove first locate", match.checkMove(location[0],location[1],testWorkerOne).equals(testWorkerOne));
+        //test spostamento prima casella
+        Assert.assertTrue("Error forceMove first locate", match.forceMove(location[0],location[1],testWorkerOne));
+        location[0] = 1;
+        location[1] = 0;
+        //test Movimento
+        Assert.assertTrue("Error checkMove locate", match.checkMove(location[0],location[1],testWorkerOne).equals(testWorkerOne));
+        //test spostamento nuova casella
+        Assert.assertTrue("Error forceMove locate", match.forceMove(location[0],location[1],testWorkerOne));
     }
     @Test
-    public void testCheckBuild() {
-
+    public void testBuildMoveOnBuild(){
+        testMovementLocateMovement();
+        int[] build = new int[]{1,1};
+        //testPosizionamento inizio partita
+        Assert.assertTrue("Error checkBuild on locate", match.checkBuild(build[0],build[1],testWorkerOne));
+        //test spostamento prima casella
+        Assert.assertTrue("Error forceBuild on locate", match.forceBuild(build[0],build[1],testWorkerOne));
+        int[] location = new int[]{1,1};
+        location[0] = 1;
+        location[1] = 1;
+        //test Movimento
+        Assert.assertTrue("Error checkMove locate", match.checkMove(location[0],location[1],testWorkerOne).equals(testWorkerOne));
+        //test spostamento nuova casella
+        Assert.assertTrue("Error forceMove locate", match.forceMove(location[0],location[1],testWorkerOne));
+        Assert.assertEquals(testWorkerOne.getCell().getBlock(), Block.B1);
     }
     @Test
     public void testNextPlayer() {
-
+        Player player1 = new Player("player1", 1, match);
+        Player player2 = new Player("player2", 2, match);
+        Player player3 = new Player("player3", 3, match);
+        match.players = new Player[]{player1, player2, player3};
+        Assert.assertTrue("Error nextPlayerActNull", match.nextPlayer().equals(player1));
+        Assert.assertTrue("Error nextPlayerAct1", match.nextPlayer().equals(player2));
+        Assert.assertTrue("Error nextPlayerAct2", match.nextPlayer().equals(player3));
+        Assert.assertTrue("Error nextPlayerAct3", match.nextPlayer().equals(player1));
     }
-    @Test
-    public void testForceMove() {
 
-    }
-    @Test
-    public void testForceBuild() {
-
-    }
-
-}*/
+}
