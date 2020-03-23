d3c92215076400c06e75ed5154b5cb126b03a63d
Leonardo Ruzza <10608001@polimi.it>
2020-03-23 16:52:38 +0100

    Implemented PlayerHestiaTest methods and fixed bug in PlayerHestia.

--- src/main/java/it/polimi/ingsw/model/PlayerHestia.java
+++ src/main/java/it/polimi/ingsw/model/PlayerHestia.java
@@ -11,22 +11,18 @@ public class PlayerHestia extends Player {
     @Override
     protected void resetTurn(){         //reset esplicito necessario nel caso venga eseguita una sola costruzione da Hestia
         firstBuild=true;
-        firstX=-1;
-        firstY=-1;
     }
 
     @Override
-    public boolean selectedWorkerMove(int x, int y){
+    public boolean selectedWorkerBuild(int x, int y){
         if(firstBuild){
             if(super.selectedWorkerBuild(x,y)){
                 firstBuild=false;
-                firstX=x;
-                firstY=y;
                 return true;
             }
             else return false;
         }
-        else if(!(x==0 || x==4 || y==0 || y==4)){             //condizione corrette per indicare caselle perimetrali?
+        else if(!(x==0 || x==4 || y==0 || y==4)){
             if(super.selectedWorkerBuild(x,y)){
                 resetTurn();
                 return true;
--- src/test/java/it/polimi/ingsw/model/PlayerHestiaTest.java
+++ src/test/java/it/polimi/ingsw/model/PlayerHestiaTest.java
@@ -1,4 +1,63 @@
 package it.polimi.ingsw.model;
 
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 public class PlayerHestiaTest {
+    private  static Match match;
+    private static PlayerHestia hestia;
+    private static Player player2Generic;
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        match=new Match(1,2);
+        hestia=new PlayerHestia("leo",2,new Card(12),match, Worker.Color.BLACK);
+        player2Generic=new Player("edo",1,new Card(1),match,Worker.Color.WHITE);
+        match.players[1] = hestia;
+        match.players[0] = player2Generic;
+
+    }
+
+    @AfterClass
+    public static void afterClass() throws Exception {
+        match=null;
+        hestia=null;
+        player2Generic=null;
+
+    }
+
+
+    @Test
+    public void testSelectedWorkerBuildAndResetTurn() {
+        Assert.assertTrue(player2Generic.setSelectedWorker(Worker.Gender.Male));
+        Assert.assertTrue(player2Generic.selectedWorkerMove(2,2));
+        Assert.assertTrue(player2Generic.setSelectedWorker(Worker.Gender.Female));
+        Assert.assertTrue(player2Generic.selectedWorkerMove(1,1));
+
+        Assert.assertTrue(hestia.setSelectedWorker(Worker.Gender.Male));
+        Assert.assertTrue(hestia.selectedWorkerMove(1,3));
+        Assert.assertTrue(hestia.setSelectedWorker(Worker.Gender.Female));
+        Assert.assertTrue(hestia.selectedWorkerMove(2,0));
+
+        Assert.assertTrue(hestia.setSelectedWorker(Worker.Gender.Female));
+        Assert.assertTrue(hestia.selectedWorkerMove(3,0));
+        Assert.assertTrue(hestia.selectedWorkerBuild(3,1));             //costruisce la prima volta con la super
+        Assert.assertTrue(hestia.selectedWorkerBuild(3,1));            //costruisce la seconda volta sulla stessa casella
+
+        Assert.assertTrue(hestia.selectedWorkerBuild(4,0));             //costruisce la prima volta con la super su una casella permimetrale
+        //hestia.resetTurn();
+        //Assert.assertTrue(hestia.selectedWorkerBuild(4,0));          //per verificare che resetTurn funzioni
+        Assert.assertFalse(hestia.selectedWorkerBuild(4,0));            //prova a costruire la seconda volta sulla stessa casella ma è perimetrale
+        hestia.resetTurn();
+
+        Assert.assertTrue(hestia.selectedWorkerBuild(3,1));     //costruisce 2 volte senza caselle perimetrali di mezzo
+        Assert.assertTrue(hestia.selectedWorkerBuild(3,1));
+
+        Assert.assertFalse(hestia.selectedWorkerBuild(3,2));    //verifico che non possa costruire troppo lontano sia alla prima costruzione che alla seconda
+        Assert.assertTrue(hestia.selectedWorkerBuild(2,1));
+        Assert.assertFalse(hestia.selectedWorkerBuild(3,2));
+
+    }
 }
