c2757a7079ee1ce8a937bb32f5dc540bf8355471
ValerioSchips <valerio.schips@mail.polimi.it>
2020-03-23 13:06:50 +0100

    PlayerPan Test

--- src/main/java/it/polimi/ingsw/model/PlayerPan.java
+++ src/main/java/it/polimi/ingsw/model/PlayerPan.java
@@ -7,14 +7,13 @@ public class PlayerPan extends Player {
     }
 
     @Override
-    public boolean checkSuperWin(){                    //attenzione bisogna che questa funzione non vega chiamata se la cella su cui si è spostato Pan è perimetrale e gioca Hera! sennò mettere qua una condizione in più
+    public boolean checkSuperWin(){
         int zOld,zFinal,groundFinal=-1;
         Block tempB;
         zOld=selectedWorker.getOldLocation().getzCoord();
         zFinal=selectedWorker.getCell().getzCoord();
         tempB=selectedWorker.getCell().getBlock();
         if(tempB == Block.B1){groundFinal++;}
-        // testare se condizione corretta!
         return (zFinal + groundFinal) - zOld <= -2;
     }
 }
