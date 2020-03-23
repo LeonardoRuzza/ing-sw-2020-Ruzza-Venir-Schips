feceba6096b8279b9c3e157f7a3c942f41e2f6ea
ValerioSchips <valerio.schips@mail.polimi.it>
2020-03-23 18:23:44 +0100

    PlayerAthena Test

--- src/main/java/it/polimi/ingsw/model/PlayerAthena.java
+++ src/main/java/it/polimi/ingsw/model/PlayerAthena.java
@@ -1,19 +1,50 @@
 package it.polimi.ingsw.model;
 
 public class PlayerAthena extends Player {
-
+    private int zOldAthena;
+    private int zNowAthena;
     public PlayerAthena(String nickname, int number, Card card, Match match, Worker.Color color) {
         super(nickname, number, card, match, color);
     }
 
     @Override
+    public boolean selectedWorkerMove(int x, int y){
+        boolean returnValue;
+        if(selectedWorker.getCell() == null){
+            return super.selectedWorkerMove(x, y);
+        }else{
+            returnValue = super.selectedWorkerMove(x, y);
+            zNowAthena = this.selectedWorker.getCell().getzCoord();
+            if(this.selectedWorker.getCell().getBlock() != null){
+                zNowAthena++;
+            }
+            zOldAthena = this.selectedWorker.getOldLocation().getzCoord();
+            if(this.selectedWorker.getOldLocation().getBlock() != null){
+                zOldAthena++;
+            }
+        }
+        return returnValue;
+    }
+
+    @Override
     public boolean checkLimitMove(Cell nextCell, Player opponent){   //ritorna true se la mossa è fattibile sennò false se la limita
         if(selectedWorker==null){ return true; }
-        int zOldAthena=this.selectedWorker.getOldLocation().getzCoord();  //il selectedWorker è unico per ogni player perciò non essendo sovrascritto rappresenta sicuramente quello del turno precedente
-        int zNowAthena=this.selectedWorker.getCell().getzCoord();
+        if(this.selectedWorker.getOldLocation() == null || this.selectedWorker.getCell() == null){
+            return true;
+        }
         if(zNowAthena-zOldAthena>=1){
+            if(opponent.selectedWorker.getCell() == null){
+                return true;
+            }
             int zOldOpponent=opponent.selectedWorker.getCell().getzCoord();
-            if(nextCell.getzCoord()-zOldOpponent>=1){
+            if(opponent.selectedWorker.getCell().getBlock() != null){
+                zOldOpponent++;
+            }
+            int zNewOpponent = nextCell.getzCoord();
+            if(nextCell.getBlock() != null){
+                zNewOpponent++;
+            }
+            if(zNewOpponent-zOldOpponent>=1){
                 return false;
             }
         }
--- src/test/java/it/polimi/ingsw/model/PlayerAthenaTest.java
+++ src/test/java/it/polimi/ingsw/model/PlayerAthenaTest.java
@@ -1,4 +1,52 @@
 package it.polimi.ingsw.model;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class PlayerAthenaTest {
+    final Card cardAthena = new Card(2);
+    final Card cardAtlas = new Card(3);
+    final int numOfPlayers = 3;
+    private Player testPlayer1;
+    private Player testPlayer2;
+    private Match match = new Match(1, numOfPlayers);
+    private Worker testWorkerAtlsas;
+    private Worker testWorkerAthena;
+    private int[] location = new int[]{4,4};
+    private int[] build = new int[]{4,3};
+    @Before
+    public void setUp() throws Exception {
+        testPlayer1 = new PlayerAtlas("player1", 1, cardAtlas, match, Worker.Color.RED);
+        testPlayer2 = new PlayerAthena("player2", 2, cardAthena, match, Worker.Color.BLACK);
+        Player player3 = new Player("player3", 3, match);
+        match.players = new Player[]{testPlayer1, testPlayer2, player3};
+        testWorkerAtlsas = testPlayer1.workers[0];
+        testWorkerAthena = testPlayer2.workers[0];
+        testPlayer2.setSelectedWorker(testWorkerAthena);
+        testPlayer1.setSelectedWorker(testWorkerAtlsas);
+        match.nextPlayer(); //turno p1;
+        match.nextPlayer(); //turno p2;
+    }
+
+    @Test
+    public void testAthenaGoUp() {
+        match.forceBuild(build[0], build[1], testWorkerAthena);
+        testPlayer2.selectedWorkerMove(location[0],location[1]);
+        location[0] = 4;
+        location[1] = 3;
+        testPlayer2.selectedWorkerMove(location[0],location[1]);
+        match.nextPlayer();
+        match.nextPlayer();
+        build[0] = 1;
+        build[1] = 1;
+        match.forceBuild(build[0], build[1], testWorkerAtlsas);
+        location[0] = 0;
+        location[1] = 0;
+        match.forceMove(location[0],location[1],testWorkerAtlsas);
+        location[0] = 1;
+        location[1] = 1;
+        match.checkMove(location[0],location[1],testWorkerAtlsas);
+        Assert.assertFalse("Error Check Limit Move", match.forceMove(location[0],location[1],testWorkerAtlsas));
+    }
 }
