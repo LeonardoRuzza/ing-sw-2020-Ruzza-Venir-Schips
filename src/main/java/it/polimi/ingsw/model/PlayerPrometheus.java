90033d429e762356a22592e491d9871d29c75042
ValerioSchips <valerio.schips@mail.polimi.it>
2020-03-24 13:18:39 +0100

    PlayerPrometheus Fix and Test

--- src/main/java/it/polimi/ingsw/model/PlayerPrometheus.java
+++ src/main/java/it/polimi/ingsw/model/PlayerPrometheus.java
@@ -1,7 +1,7 @@
 package it.polimi.ingsw.model;
 
 public class PlayerPrometheus extends Player {
-    private boolean justBuild=false;
+    private boolean justBuild=false; // mi dice se ha costruito o meno prima di muoversi
 
     public PlayerPrometheus(String nickname, int number, Card card, Match match, Worker.Color color) {
         super(nickname, number, card, match, color);
@@ -13,28 +13,42 @@ public class PlayerPrometheus extends Player {
     }
 
     @Override
+    public boolean selectedWorkerBuild(int x, int y){
+        justBuild = true;
+        return super.selectedWorkerBuild(x, y);
+    }
+
+    @Override
     public boolean selectedWorkerMove(int x, int y){  //NON BASTA ridefinire questo metodo...l'ordine di chiamata è del model/controller che dovranno chiamare nel caso per costruire prima di muoversi
-    if(!justBuild){
-        return super.selectedWorkerMove(x,y);
-    }
-    else{
-        Worker tempWorker=new Worker(selectedWorker.getGender(),selectedWorker.getColor());  //copio i valori di selected worker in un worker temporaneo
-        tempWorker.move(selectedWorker.getCell());
-        if(match.forceMove(x, y,tempWorker)){
-            if(tempWorker.getCell().getzCoord() != selectedWorker.getCell().getzCoord()){
-                match.removeWorker(tempWorker);
-                return false;
-            }
-            else{
-                match.removeWorker(tempWorker);
-                if(super.selectedWorkerMove(x,y)){
-                    resetTurn();
-                    return true;
-                }
-                else return false;
-            }
+        if(!justBuild){
+            return super.selectedWorkerMove(x,y);
         }
-        return false;
-    }
+        else{
+            Worker tempWorker=new Worker(selectedWorker.getGender(),selectedWorker.getColor());  //copio i valori di selected worker in un worker temporaneo
+            tempWorker.move(selectedWorker.getCell());
+            if(match.forceMove(x, y,tempWorker)){
+                int zNow = tempWorker.getCell().getzCoord();
+                int zOld = selectedWorker.getCell().getzCoord();
+                if(this.selectedWorker.getCell().getBlock() != null){
+                    zOld++;
+                }
+                if(tempWorker.getCell().getBlock() != null){
+                    zNow++;
+                }
+                if((zNow - zOld) > 0){
+                    match.removeWorker(tempWorker);
+                    return false;
+                }else{
+                    match.removeWorker(tempWorker);
+                    if(super.selectedWorkerMove(x,y)){
+                        resetTurn();
+                        return true;
+                    }else{
+                        return false;
+                    }
+                }
+            }
+            return false;
+        }
     }
 }
--- src/test/java/it/polimi/ingsw/model/PlayerPrometheusTest.java
+++ src/test/java/it/polimi/ingsw/model/PlayerPrometheusTest.java
@@ -1,4 +1,46 @@
 package it.polimi.ingsw.model;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class PlayerPrometheusTest {
+    final Card cardProm = new Card(8);
+    final int numOfPlayers = 3;
+    private Player testPlayer1;
+    private Match match = new Match(1, numOfPlayers);
+    private Worker testWorkerProm;
+    private int[] location = new int[]{0,0};
+    private int[] build = new int[]{1,1};
+    @Before
+    public void setUp() throws Exception {
+        testPlayer1 = new PlayerPrometheus("player1", 1, cardProm, match, Worker.Color.RED);
+        Player player2 = new Player("player2", 2, match);
+        Player player3 = new Player("player3", 3, match);
+        match.players = new Player[]{testPlayer1, player2, player3};
+        testWorkerProm = testPlayer1.workers[0];
+        testPlayer1.setSelectedWorker(testWorkerProm);
+        match.nextPlayer(); //turno p1;
+    }
+
+    @Test
+    public void buildTwoTimeTest() {
+        Assert.assertTrue("Error First Locate", testPlayer1.selectedWorkerMove(location[0],location[1]));
+        location[0] = 1;
+        location[1] = 0;
+        Assert.assertTrue("Error Build Before Move", testPlayer1.selectedWorkerBuild(build[0],build[1]));
+        Assert.assertTrue("Error First Locate", testPlayer1.selectedWorkerMove(location[0],location[1]));
+        Assert.assertTrue("Error Build After Move", testPlayer1.selectedWorkerBuild(build[0],build[1]));
+        build[0] = 1;
+        build[1] = 1;
+    }
+
+    @Test
+    public void tryGoUpTest() {
+        Assert.assertTrue("Error First Locate", testPlayer1.selectedWorkerMove(location[0],location[1]));
+        location[0] = 1;
+        location[1] = 1;
+        Assert.assertTrue("Error Build Before Move", testPlayer1.selectedWorkerBuild(build[0],build[1]));
+        Assert.assertFalse("Error First Locate", testPlayer1.selectedWorkerMove(location[0],location[1]));
+    }
 }
