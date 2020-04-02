e541d9085bdeed6dad5569d04a5ce68b1ee5d0d5
Leonardo Ruzza <10608001@polimi.it>
2020-04-02 14:21:42 +0200

    Overrided manageTurn method in PlayerAtlas.

--- src/main/java/it/polimi/ingsw/model/PlayerAtlas.java
+++ src/main/java/it/polimi/ingsw/model/PlayerAtlas.java
@@ -12,4 +12,37 @@ public class PlayerAtlas extends Player {
         }
         return false;
     }
+
+    @Override
+    public ChoiceResponseMessage manageTurn(int x, int y, Worker.Gender gender, String optional){
+        ChoiceResponseMessage tempResponse;
+        switch(stateOfTurn){
+            case 1:
+                return manageStateSelection(x, y, gender);
+            case 2:
+                tempResponse = manageStateMove(x, y);
+                if(tempResponse.getNextInstruction().equals("Ti sei mosso correttamente")){
+                    return new ChoiceResponseMessage(tempResponse.getBoard(), tempResponse.getPlayer(), tempResponse.getNextInstruction() + "Scegli dove crostruire e nel caso tu voglia costruire la cupola aggiungi DORSE dopo la casella");
+                }
+                return tempResponse;
+            case 3:
+                if(optional.equals("DORSE")){
+                        if(selectedWorkerBuildDorse(x,y)){
+                            stateOfTurn = 1;
+                            tempResponse = new ChoiceResponseMessage(match.getBoard().clone(), this, "Costruzione della cupola eseguita correttamente, il tuo turno è finito");
+                            match.nextPlayer();
+                            return tempResponse;
+                        }
+                        else return new ChoiceResponseMessage(match.getBoard().clone(), this, "Costruzione della cupola NON eseguita, cambia casella oppure rinuncia all'opzione DORSE");
+                }
+                tempResponse = manageStateBuild(x, y);
+                if(tempResponse.getNextInstruction().equals("Costruzione eseguita")){
+                    tempResponse = new ChoiceResponseMessage(tempResponse.getBoard(), tempResponse.getPlayer(), tempResponse.getNextInstruction()+ " e il tuo turno è terminato");
+                    match.nextPlayer();
+                    return tempResponse;
+                }
+                return tempResponse;
+            default: return new ChoiceResponseMessage(match.getBoard().clone(), this, "Errore nello stato del turno!"); //da valutare questo default
+        }
+    }
 }
