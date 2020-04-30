6fd845c2f93406ae69f9eacb3821dd5dcd948d81
ValerioSchips <valerio.schips@mail.polimi.it>
2020-04-30 12:42:29 +0200

    add GUI nicknamePanel with Listner

---
+++ src/main/java/it/polimi/ingsw/client/GUI/NicknamePanel.java
@@ -0,0 +1,67 @@
+package it.polimi.ingsw.client.GUI;
+
+import it.polimi.ingsw.client.Client;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class NicknamePanel extends JPanel{
+
+    BufferedImage imageBtnPlay;
+    BufferedImage imageLabelSys;
+    BufferedImage imageTxtField;
+    JLabel playButton;
+    PlayButtonListner listner;
+    JLabel imageTextBox;
+    JLabel sysLabel;
+    JTextField nicknameTxtField;
+
+    ClientGUI clientGUI;
+
+    protected NicknamePanel(ClientGUI gui) throws IOException {
+        this.clientGUI = gui;
+        imageBtnPlay = ImageIO.read(new File("src/main/resources/btn_play.png"));
+        imageLabelSys = ImageIO.read(new File("src/main/resources/sys_label_nickname.png"));
+        imageTxtField = ImageIO.read(new File("src/main/resources/nickname_txtField.png"));
+        this.setSize(new Dimension(1920,1080));
+        this.setLayout(null);
+        playButton = new JLabel(new ImageIcon(imageBtnPlay));
+        imageTextBox = new JLabel(new ImageIcon(imageTxtField));
+        sysLabel = new JLabel(new ImageIcon(imageLabelSys));
+        nicknameTxtField = new JTextField("nickname");
+
+        sysLabel.setBounds(651,188,619,207);
+
+        imageTextBox.setBounds(689,437,525,118);
+        nicknameTxtField.setBounds(715,449,470,70);
+        nicknameTxtField.setBackground(new Color(0,0,0,0));
+        nicknameTxtField.setOpaque(false);
+        nicknameTxtField.setBorder(null);
+        nicknameTxtField.setFont(new Font("ComicSansMS",Font.BOLD,30));
+        nicknameTxtField.setForeground(Color.WHITE);
+        nicknameTxtField.setHorizontalAlignment(JTextField.CENTER);
+
+        playButton.setBounds(1597,684,323,396);
+        listner = new PlayButtonListner(nicknameTxtField, playButton, clientGUI);
+        playButton.addMouseListener(listner);
+
+        this.setOpaque(true);
+        this.setBackground(new Color(0,0,0,0));
+        this.add(nicknameTxtField);
+        this.add(imageTextBox);
+        this.add(sysLabel);
+        this.add(playButton);
+    }
+
+    public void showNameAlreadyTokenDialog(JFrame f){
+        int dialogResult = JOptionPane.showOptionDialog(f, "This name is already taken", "Nickname Error", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, null,"Cancel");
+        if(dialogResult == JOptionPane.DEFAULT_OPTION){
+            nicknameTxtField.setText("nickname");
+            playButton.addMouseListener(listner);
+        }
+    }
+}
---
+++ src/main/java/it/polimi/ingsw/client/GUI/PlayButtonListner.java
@@ -0,0 +1,50 @@
+package it.polimi.ingsw.client.GUI;
+
+import jdk.tools.jlink.internal.Jlink;
+
+import javax.swing.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class PlayButtonListner extends JFrame implements MouseListener {
+
+    ClientGUI gui;
+    JTextField nicknameTxtField;
+    JLabel playBtn;
+
+    public PlayButtonListner(JTextField txtField, JLabel btn, ClientGUI gui){
+        this.gui = gui;
+        this.nicknameTxtField = txtField;
+        this.playBtn = btn;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        if(nicknameTxtField != null){
+            if(nicknameTxtField.getText() != null){
+                gui.outcomeGUI.add(nicknameTxtField.getText());
+                playBtn.removeMouseListener(this);
+            }
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+}
