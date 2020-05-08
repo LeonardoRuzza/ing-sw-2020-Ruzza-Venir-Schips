7bd8617c104ee75028e56b49ff964be98e3040ef
Leonardo Ruzza <10608001@polimi.it>
2020-05-08 14:39:03 +0200

    Fixed a bug in PlayerHera.

--- src/main/java/it/polimi/ingsw/model/PlayerHera.java
+++ src/main/java/it/polimi/ingsw/model/PlayerHera.java
@@ -12,10 +12,11 @@ public class PlayerHera extends Player {
      * @return {@code true} if the normal winning conditions is possible; {@code false} otherwise
      */
     @Override
-    public boolean checkLimitWin(Player opponent){              //in match questo metodo dovrà essere chiamato mandando il player attuale (non se è Hera stessa)
+    public boolean checkLimitWin(Player opponent){
+        if(opponent.equals(this)) return true;
         int x=opponent.selectedWorker.getCell().getxCoord();
         int y=opponent.selectedWorker.getCell().getyCoord();
-        return (x != 0 && x != 4 && y != 0 && y != 4);             //ritorna false se il giocatore non può vincere
+        return (x != 0 && x != 4 && y != 0 && y != 4);
     }
 }
 
