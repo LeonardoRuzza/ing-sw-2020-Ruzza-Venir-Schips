5dd4847798ae1f19cb83edf9f4b50341ecc2ea87
Leonardo Ruzza <10608001@polimi.it>
2020-05-14 12:32:42 +0200

    Added JavaDoc.

--- src/main/java/it/polimi/ingsw/model/ChoiceResponseMessage.java
+++ src/main/java/it/polimi/ingsw/model/ChoiceResponseMessage.java
@@ -3,21 +3,40 @@ package it.polimi.ingsw.model;
 public class ChoiceResponseMessage {
     private final Player player;
     private final Match match;
-    private String nextInstruction;
+    private final String nextInstruction;
 
+    /**Create a ChoiceResponseMessage.
+     * <p>
+     * @param match the match associated
+     * @param player the player who caused the interaction with the model
+     * @param instruction the Sting which contains info of the result derived by the model
+     */
     ChoiceResponseMessage(Match match, Player player, String instruction) {
         this.player = player;
         this.match = match;
         this.nextInstruction = instruction;
     }
 
+    /**Getter for the Player of ChoiceResponseMessage.
+     * <p>
+     * @return {@code Player}
+     */
     public Player getPlayer() {
         return player;
     }
 
+    /**Getter for the Match of ChoiceResponseMessage.
+     * <p>
+     * @return {@code Match}
+     */
     public Match getMatch() {
         return match;
     }
+
+    /**Getter for the nextInstruction of ChoiceResponseMessage.
+     * <p>
+     * @return {@code String}
+     */
     public String getNextInstruction() {
         return nextInstruction;
     }
--- src/main/java/it/polimi/ingsw/model/PlayerChoiceMessage.java
+++ src/main/java/it/polimi/ingsw/model/PlayerChoiceMessage.java
@@ -10,6 +10,15 @@ public class PlayerChoiceMessage {
     private final View view;
     private final String optional;
 
+    /**Create a new PlayerChoiceMessage.
+     * <p>
+     * @param player the player who requires the interaction
+     * @param x coordinate x
+     * @param y coordinate y
+     * @param view the View of the player
+     * @param optional the optional message attached from the request coming from client-side (eventually empty)
+     * @param gender the gender of the worker (can be null)
+     */
     public PlayerChoiceMessage(Player player, int x, int y, View view, String optional, Worker.Gender gender) {
         this.player = player;
         this.x = x;
@@ -19,26 +28,50 @@ public class PlayerChoiceMessage {
         this.gender = gender;
     }
 
+    /**Getter for coordinate x.
+     * <p>
+     * @return {@code int}
+     */
     public int getX() {
         return x;
     }
 
+    /**Getter for coordinate y.
+     * <p>
+     * @return {@code int}
+     */
     public int getY() {
         return y;
     }
 
+    /**Getter for the player.
+     * <p>
+     * @return {@code Player}
+     */
     public Player getPlayer() {
         return player;
     }
 
+    /**Getter for the view.
+     * <p>
+     * @return {@code View}
+     */
     public View getView() {
         return view;
     }
 
+    /**Getter for the optional.
+     * <p>
+     * @return {@code String}
+     */
     public String getOptional() {
         return optional;
     }
 
+    /**Getter for the gender (Worker.Gender).
+     * <p>
+     * @return {@code Worker.Gender}
+     */
     public Worker.Gender getGender() {
         return gender;
     }
